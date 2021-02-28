f97d2bad79c52b330cfc5ea1106f4a7f57891eb2
Amanda Hartung <ahartung@leaporbit.com>
2021-02-28 20:58:32 +0000

    add theme

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import { Button } from "@material-ui/core";
 
 export default function App() {
   return (
-    <Button variant="contained">Increment Counter</Button>
+    <Button
+      variant="contained"
+      color="primary"
+    >
+      Increment Counter
+    </Button>
   );
 }
